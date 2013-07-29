7826f7a5b1b2d4202a5ca16dafb8d65ac84d5b2f
tdouglas <tdouglas@seamlessTL.com>
2013-07-29 17:19:49 -0400

    refactored unfinished section with help from Max

--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,49 @@
+//cloned whenever we want to append a new item
+var toDoItem = {
+  // receives text and sets text as task's name
+  setTaskName: function(text) {
+    this.taskName = text;
+  },
+  // like create task (creates object, sets name, then passes it on)
+  render: function() {
+    var listItem = document.createElement('li');
+    listItem.innerHTML = this.taskName;
+    return listItem;
+  }
+};
+
+// find diff lists and append to DOM
+// gives item to toDoItem
+var toDoApp = {
+  getUnfinished: function(){
+    return document.getElementById('unfinished');
+  },
+  createTask: function(text) {
+    //clones to do item when passed in
+  var task = Object.create(toDoItem);
+  // calling set task name method
+  task.setTaskName(text);
+  this.appendTask(task);
+  },
+  appendTask: function(task) {
+  this.getUnfinished().appendChild(task.render());
+  }
+};
 
 
-function ToDoApp(unfinishedItems, finishedItems) {
-  this.unfinishedItems = unfinishedItems;
-  this.finishedItems = finishedItems;
-}
 
-function ToDoItem(name, description) {
-  this.name = name;
-  this.description = description;
-}
+window.onload = function(){
+
+  var button = document.getElementById('add-item');
+  button.onclick = function () {
+  var input = document.getElementById('new-task-field').value;
+  toDoApp.createTask(input);
+  };
+
+};
+
+
+
 
 if (typeof Object.create !== 'function') {
   Object.create = function (o) {
